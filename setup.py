520e1f8d30751fc45546b06dea74d71140d133e5
Jack Frost <j4qfrost@gmail.com>
2019-09-25 02:46:13 -0700

    uptick

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='posenet-torch',
-    version='0.0.1',
+    version='0.0.2',
     packages=['posenet','posenet.converter', 'posenet.models'],
     package_data={'': ['converter/config.yaml']},
 	include_package_data=True,
