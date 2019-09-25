6ce0c3109420306fcea08a53f5843370c5f6ef77
Jack Frost <j4qfrost@gmail.com>
2019-09-25 02:44:59 -0700

    add models logic

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='posenet-torch',
     version='0.0.1',
-    packages=['posenet','posenet.converter'],
+    packages=['posenet','posenet.converter', 'posenet.models'],
     package_data={'': ['converter/config.yaml']},
 	include_package_data=True,
 )
